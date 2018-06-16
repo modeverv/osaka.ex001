296c0dd95788349c3daeb70b35e9a39b2e5453ab
modeverv <modeverv@gmail.com>
2018-06-17 01:12:05 +0900

    2018-06-17 01:12:05

--- scorematch/lib/kv/registry.ex
+++ scorematch/lib/kv/registry.ex
@@ -3,30 +3,42 @@ defmodule KV.Registry do
 
   # Client API
 
-  def start_link opts \\ [] do
-    GenServer.start_link __MODULE__, :ok, opts
-  end
+#   def start_link opts \\ [] do
+#     GenServer.start_link __MODULE__, :ok, opts
+#   end
+# 
+#   def lookup server, name do
+#     GenServer.call server, {:lookup, name}
+#   end
+# 
+#   def create server, name do
+#     GenServer.cast server, {:create, name}
+#   end
 
+  def start_link manager, opts \\ [] do
+    GenServer.start_link __MODULE__, manager, opts
+  end
+ 
   def lookup server, name do
     GenServer.call server, {:lookup, name}
   end
-
+ 
   def create server, name do
     GenServer.cast server, {:create, name}
   end
 
-  # Server Callbacks
-
-  def init :ok do
-    names = HashDict.new
-    refs = HashDict.new
-    {:ok, {names, refs}}
-#     {:ok, HashDict.new}
-  end
-
-  def handle_call {:lookup, name}, _from, {names, _} = state do
-    {:reply, HashDict.fetch(names, name), state}
-  end
+#   # Server Callbacks
+# 
+#   def init :ok do
+#     names = HashDict.new
+#     refs = HashDict.new
+#     {:ok, {names, refs}}
+# #     {:ok, HashDict.new}
+#   end
+# 
+#   def handle_call {:lookup, name}, _from, {names, _} = state do
+#     {:reply, HashDict.fetch(names, name), state}
+#   end
 
 #   def handle_call {:lookup, name}, _from, names do
 # 		{:reply, HashDict.fetch(names, name), names}
@@ -41,27 +53,63 @@ defmodule KV.Registry do
 #     end
 #   end
 
-  def handle_cast {:create, name}, {names, refs} do
-    if HashDict.get names, name do
-      {:reply, {names, refs}}
+#   def handle_cast {:create, name}, {names, refs} do
+#     if HashDict.get names, name do
+#       {:reply, {names, refs}}
+#     else
+#       {:ok, bucket} = KV.Bucket.start_link
+#       ref = Process.monitor bucket
+#       refs = HashDict.put refs, ref, name
+#       names = HashDict.put names, name, bucket
+#       {:noreply, {names, refs}}
+#     end
+#   end
+# 
+#   def handle_info {:DOWN, ref, :process, _pid, _reason}, {names, refs} do
+#     {name, refs} = HashDict.pop refs, ref
+#     names = HashDict.delete names, name
+#     {:noreply, {names, refs}}
+#   end
+# 
+#   def handle_info msg, state do
+#     {:noreply, state}
+#   end
+
+  # Server API
+
+  def init events do
+    names = HashDict.new
+    refs = HashDict.new
+    {:ok, %{names: names, refs: refs, events: events}}
+  end
+
+  def handle_call {:lookup, name}, _from, state do
+    {:reply, HashDict.fetch(state.names, name), state}
+  end
+
+  def handle_cast {:create, name}, state do
+    if HashDict.get state.names, name do
+      {:reply, {state.names, state.refs}}
     else
       {:ok, bucket} = KV.Bucket.start_link
       ref = Process.monitor bucket
-      refs = HashDict.put refs, ref, name
-      names = HashDict.put names, name, bucket
-      {:noreply, {names, refs}}
+      refs = HashDict.put state.refs, ref, name
+      names = HashDict.put state.names, name, bucket
+      GenEvent.sync_notify state.events, {:create, name, bucket}
+      {:noreply, %{state | names: names, refs: refs}}
     end
   end
 
-  def handle_info {:DOWN, ref, :process, _pid, _reason}, {names, refs} do
-    {name, refs} = HashDict.pop refs, ref
-    names = HashDict.delete names, name
-    {:noreply, {names, refs}}
+  def handle_info {:DOWN, ref, :process, _pid, _reason}, state do
+    {name, refs} = HashDict.pop state.refs, ref
+    names = HashDict.delete state.names, name
+    GenEvent.sync_notify state.events, {:exit, name, _pid}
+    {:noreply, %{state | names: names, refs: refs}}
   end
-
+ 
   def handle_info msg, state do
     {:noreply, state}
   end
-
+	
 end
  
--- scorematch/lib/scorematch.ex
+++ scorematch/lib/scorematch.ex
@@ -1,3 +1,11 @@
+defmodule Forwarder do
+  use GenEvent
+  def handle_event event, parent do
+    send parent, event
+    {:ok, parent}
+  end
+end
+
 defmodule Scorematch do
   @moduledoc """
   Documentation for Scorematch.
@@ -17,18 +25,32 @@ defmodule Scorematch do
   end
 
   def main args do
-    {:ok, registry} = KV.Registry.start_link
-    KV.Registry.create registry, "card1"
-    {:ok, bucket} = KV.Registry.lookup registry, "card1"
-    Agent.stop bucket
-    {:ok, bucket} = KV.Registry.lookup registry, "card1"
-    IO.inspect Process.alive? bucket
+#     {:ok, registry} = KV.Registry.start_link
+#     KV.Registry.create registry, "card1"
+#     {:ok, bucket} = KV.Registry.lookup registry, "card1"
+#     Agent.stop bucket
+#     {:ok, bucket} = KV.Registry.lookup registry, "card1"
+#     IO.inspect Process.alive? bucket
 
 #     {:ok, registry} = KV.Registry.start_link
 #     KV.Registry.create registry, "card1"
 #     {:ok, bucket} = KV.Registry.lookup registry, "card1"
 #     KV.Bucket.put bucket, "園田ことり", "いくよFull Combo!"
 #     IO.puts KV.Bucket.get bucket, "園田ことり"
+
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = KV.Registry.start_link manager
+    GenEvent.add_mon_handler manager, Forwarder, self()
+    KV.Registry.create registry, "card1"
+    {:ok, bucket} = KV.Registry.lookup registry, "card1"
+    receive do
+      {:create, "card1", ^bucket} -> IO.puts "create:" <> Kernel.inspect bucket
+    end
+    Agent.stop bucket
+    receive do
+      {:exit, "card1", ^bucket} -> IO.puts "exit:" <> Kernel.inspect bucket
+    end
+
   end
 
 
